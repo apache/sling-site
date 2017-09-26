19b0d30d48f8777de5c6a6c9bca52d4fd382ff59
Bertrand Delacretaz <bdelacretaz@apache.org>
2017-09-26 17:32:16 +0200

    Default BASE

--- ARCHIVE/conversion-scripts/open-all-pages.sh
+++ ARCHIVE/conversion-scripts/open-all-pages.sh
@@ -1,6 +1,7 @@
 export SRC="src/main/jbake/content"
 export PAT=".*jbake.*content"
-export BASE="http://localhost:8820"
+export BASE=${BASE:-"http://localhost:8820"}
+echo "Using base URL $BASE"
 
 find $SRC -type f | sort | sed 's/\.md$/\.html/' | sed "s/${PAT}//" | while read f
 do
