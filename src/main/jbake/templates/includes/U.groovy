e21451fabcb1e7d36164b54f0393181b7f08d3f2
Bertrand Delacretaz <bdelacretaz@apache.org>
2017-11-23 12:51:34 +0100

    Move common template utilities to their own file

---
+++ src/main/jbake/templates/includes/U.groovy
@@ -0,0 +1,75 @@
+// Shared utilities
+package includes
+
+class U {
+    def processBody(content, config) {
+    	def str = content.body
+
+    	// Temporarily disable the TOC macro, replace with a comment
+    	def replacement ='<!-- TODO reactivate TOC once JBake moves to flexmark-java -->\n'
+    	str = str.replaceAll('\\[TOC\\]', replacement)
+
+    	// Temporarily disable the syntax markers (of which there are two flavors, for some reason)
+    	str = str.replaceAll('(::|#\\!)(java|jsp|xml|sh|javascript|html) *\\n', '<!-- TODO syntax marker ($1$2) disabled -->')
+
+    	// Optionally expand variables
+    	if("true".equals(content.expandVariables)) {
+    		str = expandVariables(str, config)
+    	}
+    	return str
+    }
+
+    def expandVariables(str, config) {
+        def pageVariables = [
+            sling_tagline : config.blog_subtitle,
+            sling_minJavaVersion : "8",
+            sling_minMavenVersion : "3.5.0",
+            sling_releaseVersion : "9"
+        ]
+
+    	// Use a closure to avoid exception on missing variable
+    	str = str.replaceAll(/\$\{(\w+)\}/) { key -> pageVariables[key[1]] ?: "MISSING_PAGE_VARIABLE:${key[0]}" }
+    }
+
+    // Is parent an ancestor of child?
+    def isAncestor(parent, child) {
+    	// assuming .html extension
+    	return child.uri[0..-6].contains(parent.uri[0..-6]) && !child.uri.equals(parent.uri)
+    }
+
+    // Return the parents of supplied content, sorted by
+    // their depth in the tree, root first
+    def getSortedParents(content, published_content) {
+    	def result = new TreeMap()
+    	result.put(0, [ uri:"", title:"Home" ])
+    	published_content.each { item ->
+    		if(isAncestor(item, content)) {
+    			result.put(item.uri.length(), item)
+    		}
+    	}
+    	return result
+    }
+
+    def exec(cmd, defaultText) {
+    	try {
+     	    def p = cmd.execute()
+    	    p.waitFor()
+      	    return p.text
+    	} catch(Exception e) {
+    		return defaultText
+    	}
+    }
+
+    def getRevisionInfo(filename) {
+        def lastCommit = "444eb637ff1ddcf11a0f37f02dd4b3fe89eb149f"
+    	def gitCmd = 'git log -1 --format=%h####%ad####%an####%s ' + filename
+    	def defaultText = "0####0000####<MISSING>####<MISSING>"
+    	def gitInfo = exec(gitCmd, defaultText).split("####")
+    	return [
+    		lastCommit : gitInfo[0],
+    		date : gitInfo[1],
+    		author : gitInfo[2],
+    		comment : gitInfo[3]
+    	]
+    }
+}
