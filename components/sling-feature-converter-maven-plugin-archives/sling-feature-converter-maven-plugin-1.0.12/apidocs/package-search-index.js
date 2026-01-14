34f19ec461168bed151a81b02fda4e6e0a1499c9
Stefan Seifert <stefanseifert@users.noreply.github.com>
2026-01-14 13:59:48 +0100

    sling-feature-converter-maven-plugin 1.0.12

---
+++ components/sling-feature-converter-maven-plugin-archives/sling-feature-converter-maven-plugin-1.0.12/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.apache.sling.cpconverter.maven.mojos"},{"l":"org.apache.sling.sling_feature_converter_maven_plugin"}];updateSearchResults();
